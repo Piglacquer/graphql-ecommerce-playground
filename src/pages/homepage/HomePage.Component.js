44738713ea843df71ece9e8163cea4c28ce07b12
pbiffle <patrick.biffle@dish.com>
2021-05-12 14:48:33 -0700

    added Directory, MenuItem, HomePage components

---
+++ src/pages/homepage/HomePage.Component.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './homepage.styles.scss';
+import Directory from '../../components/directory/Directory.Component';
+
+const HomePage = () => (
+	<div className='homepage'>
+		<Directory />
+	</div>
+);
+
+export default HomePage;
